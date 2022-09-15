f184724d0db3ef82bc1d582e74028af2d8e34d20
DMZellmer <dave.zellmer17@gmail.com>
2022-09-15 11:40:02 -0500

    push

--- quiz-backend/src/main/java/net/yorksolutions/quizbackend/QuizService.java
+++ quiz-backend/src/main/java/net/yorksolutions/quizbackend/QuizService.java
@@ -1,9 +1,17 @@
 package net.yorksolutions.quizbackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class QuizService {
@@ -15,16 +23,35 @@ public class QuizService {
         this.repo = repo;
         this.restTemplate = new RestTemplate();
     }
-    public void create(){
 
+    public Boolean checkAuth(UUID token, String role) {
+        try {
+            ResponseEntity<Boolean> response = this.restTemplate.getForEntity("http://localhost:8080/checkAuth/" + token + "?role=" + role, Boolean.class);
+            return response.getBody();
+        } catch (RestClientException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
-    public Iterable getQuizList(){
+
+    public void createQuiz(@RequestBody Quiz quiz) {
+        if (repo.existsByTitle(quiz.title)) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Quiz with title " + quiz.title + " already exists");
+        } else {
+            repo.save(quiz);
+        }
+    }
+
+    public Iterable getQuizList() {
         return repo.findAll();
     }
-    public void editQuiz(){
 
+    public void editQuiz(Quiz quiz) {
+        this.repo.save(quiz);
     }
-    public void deleteQuiz(){
 
+    public void delete(Long id) {
+        Optional<Quiz> quiz = this.repo.findById(id);
+        if (quiz.isEmpty())
+            throw new ResponseStatusException(HttpStatus.GONE);
     }
 }
